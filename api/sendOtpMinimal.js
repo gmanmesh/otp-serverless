11370c19a73b42a35921f0d3f106cf4f0af3a0bc
gmanmesh <140646198+gmanmesh@users.noreply.github.com>
2025-07-12 09:36:53 -0500

    Update sendOtpMinimal.js

--- api/sendOtpMinimal.js
+++ api/sendOtpMinimal.js
@@ -1,4 +1,4 @@
-import { createClient } from '@supabase/supabase-js';
+/*import { createClient } from '@supabase/supabase-js';
 import fetch from 'node-fetch';
 
 const SUPABASE_URL = process.env.SUPABASE_URL;
@@ -6,6 +6,7 @@ const SUPABASE_API_KEY = process.env.SUPABASE_API_KEY;
 const BREVO_API_KEY = process.env.BREVO_API_KEY;
 
 const supabase = createClient(SUPABASE_URL, SUPABASE_API_KEY);
+*/
 
 export default async function handler(req, res) {
   console.log('Handler start');
