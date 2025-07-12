a81850fe57c3fe47d43ab90659c60d11f7fc445a
gmanmesh <140646198+gmanmesh@users.noreply.github.com>
2025-07-12 09:25:13 -0500

    Update sendOtpMinimal.js

--- api/sendOtpMinimal.js
+++ api/sendOtpMinimal.js
@@ -1,4 +1,13 @@
 export default async function handler(req, res) {
   console.log('Handler start');
-  res.status(200).json({ message:"Successfully Started"});
+  console.log('Method:', req.method);
+    if (req.method !== 'POST') {
+      console.log('Invalid method');
+      res.status(405).json({ error: 'Method Not Allowed' });
+      return;
+    }
+
+    const { email } = req.body;
+    console.log('Received email:', email);
+  res.status(200).json({ message:"Successfully Started", email: email});
 }
